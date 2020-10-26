4d75eb2711779cc043c4c0c1096084edbca41eb3
marco <marcodecrema@libero.it>
2020-10-26 23:39:32 +0100

    prova 1

---
+++ database/migrations/2020_10_26_220234_create_students_table.php
@@ -0,0 +1,35 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+class CreateStudentsTable extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('students', function (Blueprint $table) {
+            $table->id();
+            $table->string("name", 30);
+            $table->string("lastname", 30);
+            $table->date("date_of_birth", 1);
+            $table->string("gender", 1);
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('students');
+    }
+}
